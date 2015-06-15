3134516ae240b15a7474209bf2b818d9f8a0cde9
etheleon <picy2k@gmail.com>
2015-06-15 17:39:52 +0800

    fixed csv

--- kegg.0500.igraphMetabolism.r
+++ kegg.0500.igraphMetabolism.r
@@ -4,17 +4,17 @@ library(magrittr)
 library(igraph)
 
 args = commandArgs(T)
-# output eg. ~/newMeta4j/misc/
+args = "~/newMeta4j2/misc/"
 
-relationships <- list.files(args[1])     %>%
-grep("rels$", ., value=T)                            %>%
-paste(args[1], ., sep="/")                           %>%
-lapply(function(fileName) read.table(fileName, h=T)) %>%
-do.call(rbind,.)                                     %>%
-unique
+relationships <- list.files(args[1])                                  %>%
+    grep("rels$", ., value=T)                            %>%
+    paste(args[1], ., sep="/")                           %>%
+    lapply(function(fileName) read.table(fileName, h=T)) %>%
+    do.call(rbind,.)                                     %>%
+    unique
 
 
 ## Creates IGRAPH OBJ
 wholeMetabolism = graph.data.frame(relationships[,1:2], directed=T)
-save(wholeMetabolism, file=sprintf("%s/wholeMetabolism.rda", args[1])
+save(wholeMetabolism, file=sprintf("%s/wholeMetabolism.rda", args[1]))
 
