c9fdc22ae2e5c70d149c86089bb8b3a42834a227
etheleon <picy2k@gmail.com>
2017-03-20 14:34:54 +0800

    Fix arg length error

--- kegg.0200.cpd_nodedetails.pl
+++ kegg.0200.cpd_nodedetails.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 
 use strict;
-die "USAGE: $0 KEGGftp/ligand/compound/compound" unless $#ARGV == 0;
+die "USAGE: $0 KEGGftp/ligand/compound/compound" unless $#ARGV == 1;
 
 $/ = '///';
 
